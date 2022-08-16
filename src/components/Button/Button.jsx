a38578045c60ea28f66bd9a76b180e809ef6f4ae
yaan0 <yasha8600@bk.ru>
2022-08-16 17:15:47 +0300

    added to component input, button, select and checkBox

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+
+
+
+import { styled } from '@mui/material/styles';
+import Button from "@mui/material/Button";
+
+
+
+const BlackButton = styled(Button)({
+  backgroundColor: "#363636",
+  padding: '6px 12px',
+  color: 'white',
+  borderRadius: 'initial',
+  '&:hover': {
+    backgroundColor: '#363636',
+    borderColor: '#363636',
+    boxShadow: 'none',
+  },
+  '&:active': {
+    boxShadow: 'none',
+    backgroundColor: '#363636',
+    borderColor: '#363636',
+  },
+  '&:focus': {
+    boxShadow: '#363636',
+  },
+
+})
+
+const BasicButtons = ({disabled, text,color, size }) => {
+
+  return (
+    <BlackButton
+      color={color === 'error' ? 'error' : 'primary'}
+      size={size}
+      disabled={disabled}
+    >
+      {text}
+    </BlackButton>
+  )
+}
+
+export default BasicButtons;
